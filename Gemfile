abf5ec5de978ff3b52b7a8ef6fe20626a2e7354a
Guillaume954 <guillaume.farina95@gmail.com>
2019-10-09 17:44:07 +0200

    bot ok

--- Gemfile
+++ Gemfile
@@ -2,7 +2,12 @@
 
 source "https://rubygems.org"
 
+=begin 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+=end
 
 # gem "rails"
 gem 'dotenv'
+gem 'twitter'
+gem 'pry'
+gem 'rubocop'
--- lib/app.rb
+++ lib/app.rb
@@ -2,17 +2,18 @@ require 'twitter'
 
 require 'dotenv'# Appelle la gem Dotenv
 
-Dotenv.load('.env')
+Dotenv.load('../.env')
 
 def login_twitter
-client = Twitter::REST::Client.new do |config|
+  $client = Twitter::REST::Client.new do |config|
    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
- end
- return client
- client.update('Hello le monde !!!!')
+	end
 end
 
 login_twitter
+
+$client.update(" Bisous LORIANNE !!!")
+
